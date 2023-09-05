66a4c48d60902edc17ceb4bbf947f86512013855
chandramohan-c <89748935+chandramohan-c@users.noreply.github.com>
2023-09-05 03:07:38 -0500

    reverted stages info

--- test.js
+++ test.js
@@ -7,12 +7,12 @@ export const options = {
     duration: '1m',
     vus: 5,
 
-    stages: [
-        { duration: '1m', target: 20 },
-        { duration: '3m', target: 20 },
-        { duration: '1m', target: 0 },
-      ],
-      
+   // stages: [
+    //    { duration: '1m', target: 20 },
+    //    { duration: '3m', target: 20 },
+    //    { duration: '1m', target: 0 },
+   //   ],
+
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
         http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
